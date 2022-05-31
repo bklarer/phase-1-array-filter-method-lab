fc7fe07d60955c0613f5b80a16638cbca7249b64
Benjamin Klarer <benjaminklarer@gmail.com>
2022-05-30 23:00:16 -0700

    commit tree 21eb2d1428e9e0518d801b9f155a9d38ece7ca0e

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, name) {
+    return array.filter(element => element.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(array, name) {
+    return array.filter(element => element.startsWith(name))
+}
+
+function matchName(array, name) {
+    return array.filter(element => element.name === name)
+}
